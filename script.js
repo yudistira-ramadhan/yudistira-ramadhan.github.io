7db397a0ef1d5d3115f009a1904e401f5916801f
Yudist <yudist1997@gmail.com>
2023-12-10 00:01:33 +0700

    change event date

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you tomorrow!";
+  question.innerHTML = "Yay, see you on Sunday!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
   document.querySelector(".btn-group").style.display = "none";
